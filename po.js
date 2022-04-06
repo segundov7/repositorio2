02cc22d5d77194278b4d02f2b82941e371484dd9
segundo vallejos <segundovallejos@gmail.com>
2022-04-05 21:46:51 -0300

    funciones

--- po.js
+++ po.js
@@ -1,6 +1,6 @@
 //______________________________________________IF, ELSE IF, ELSE_______________________________________________
 /*let dineroPedro = prompt("cunato dinero tenes pedro?");
-parseInt(dineroPedro)
+parseInt(dineroPedro)                              //parseInt es para que me devuelva numeros
 
 if(dineroPedro >= 10 && dineroPedro <20){
     document.write("comprate esta, que cuesta 10");
@@ -35,11 +35,11 @@ let caja = {
 let animal = caja["animal"];
 let prenda = caja["prenda"];
 let talle = caja["talle"];
-let deporte = caja["deporte"]
+let deporte = caja["deporte"];
 
 let frase = `mi ${animal} tiene una ${prenda} <br> del talle ${talle} y juega al ${deporte}`;
 
-document.write(frase)
+document.write(frase);
 */
 //______________________________________________________WHILE_____________________________________________________
 /*
@@ -48,12 +48,12 @@ let numero = 0;
 while(numero < 10){
     numero ++;
 
-    document.write(numero + "<br>")
+    document.write(numero + "<br>");
 }
 */
 //___________________________________________________DO WHILE____________________________________________________
 /*
-let numero = 0
+let numero = 0;
 
 do{
     document.write(numero + "<br>");
@@ -64,17 +64,157 @@ while (numero < 10);
 */
 //_____________________________________________________BREAK____________________________________________________
 /*
-let numero = 0
+let numero = 0;
 
 while(numero < 1000){
     numero ++;
-    document.write(numero)
+    document.write(numero);
     if(numero == 17){
         break;
     }
 }
 
-document.write(" fin")
+document.write(" fin");
 */
 //______________________________________________________FOR_____________________________________________________
+//FOR: 3 PARTES: CREAMOS LA VARIABLE, LE DAMOS LA CONDICION Y LA AUMENTAMOS(++) O LA DISMINUIMOS(--)
+/*
+for( let i = 1; i <= 10; i++){
+    document.write(i + "<br>");
+ 
+}
+*/
+//________________________________________________FOR IN, FOR OF ________________________________________________
+// FOR IN ME MUESTRA LA POSICION Y FOR OF ME MUESTRA EL VALOR DE LA POSICION
+/*
+let animales = ["gato", "perro", "ardila", "elefante", "vaca"]
 
+for(let animal in animales){
+    document.write(animal + "<br>")
+}
+document.write("<br>")
+
+for(let animal of animales){
+    document.write(animal + "<br>")
+}
+*/
+//_____________________________________________________LABEL______________________________________________________
+// VOLVER A REPASAR PORQUE NO ENTENDI UNA MIERDA
+/*
+let array1 = ["pedro", "marcelo","carlos"];
+let array2 = ["carla", "maria", array1 , "josefina"]
+
+for( let array in array2){
+    if(array == 2){
+        for( array of array1){
+            document.write(array + "<br>")
+        }
+    }
+    else{
+        document.write(array2[array] + "<br>")
+    }
+
+}
+*/
+//____________________________________________________FUNCIONES___________________________________________________
+/*
+
+//___FORMA 1____
+
+function comida(){
+    let comer = prompt("que queres comer pibe?");
+    if (comer == "fideos"){
+        alert("no tenemos fideos pibe");
+    }else if( comer == "una hamburguer"){
+        alert("dale tenemos la de chedar que es una deliciura pibe");
+    }else{
+        alert("te vas a tener que ir a otro lado pibe");
+
+    }
+}
+
+comida();
+comida();
+comida();
+
+// ___FORMA 2___
+
+comida = function(){
+    let comer = prompt("que queres comer pibe?");
+    if (comer == "fideos"){
+        alert("no tenemos fideos pibe");
+    }else if( comer == "una hamburguer"){
+        alert("dale tenemos la de chedar que es una deliciura pibe");
+    }else{
+        alert("te vas a tener que ir a otro lado pibe");
+
+    }
+    
+}
+comida();
+comida();
+comida();
+*/
+//__________________________________________________RETURN_______________________________________________________
+/*
+//REPASAR
+
+function saludar(){
+    alert("hola");
+    return 8;
+}
+
+let saludo = saludar();
+document.write(saludo);
+*/
+//________________________________________________PARAMETROS_____________________________________________________
+/*
+function sumar (num1, num2){
+    let res = num1 + num2;
+    document.write(res)
+}
+sumar(84,14)
+*/
+/*
+//REPASAR
+function sumar(num1, num2){
+    let res = num1 + num2;
+    return res;
+}
+let resultado = sumar(15,20)
+
+
+document.write(resultado)
+*/
+/*
+function saludar (nombre){
+    let saludo = `¡Hola ${nombre}!, ¿Como estas?`;
+    document.write(saludo)
+}
+saludar("mariano de la canal")
+*/
+//_______________________________________________FUNCION FLECHA__________________________________________________
+//EN LA FUNCION FLECHA SI USAMOS UN SOLO PARAMETRO NO HACE FALTA PONERLE PARENTESIS
+/*
+const saludar = nombre=>{
+    let saludo = `hola ${nombre}, como estas?`;
+    document.write(saludo + "<br>");
+}
+ saludar("pedro")
+ saludar("carlos")
+ saludar("segundo")
+ saludar("marcos")
+ saludar("marcelo")
+ saludar("pablo")
+ */
+
+/*
+const saludar = nombre => document.write(`hola ${nombre} como estas?` +"<br>");
+
+saludar("pedro")
+saludar("carlos")
+saludar("segundo")
+saludar("marcos")
+saludar("marcelo")
+saludar("pablo")
+*/
